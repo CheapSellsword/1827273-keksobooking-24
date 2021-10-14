166301e831c9ad77e75874a24b7b840dacbeb8d0
Al Much <almuchwork@gmail.com>
2021-10-14 18:01:14 +0300

    Добавил функции для создания массива из 10 объявлений. Есть вопросы насчет ESLint.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,160 @@
-// Решения нашел здесь:
-// https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-// https://www.codegrepper.com/code-examples/javascript/one+digit+after+decimal+point+javascript+random+number
+// <<<Функции для получения случайных чисел>>>
+
+function getRandomPositiveFloat (first, second, digits = 1) {
+
+  const lower = Math.min(Math.abs(first), Math.abs(second));
+  const upper = Math.max(Math.abs(first), Math.abs(second));
+
+  const result = Math.random() * (upper - lower) + lower;
+
+  return result.toFixed(digits);
+}
+
+function getRandomPositiveInteger (first, second) {
+
+  const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
+  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
 
 
-// Если передать значение «до» меньшее, чем значение «от», то они поменяются местами с помощью функции swapAndRoundNums. Дополнительно, эта функция округляет числа.
+// <<<Функции для получения 10 не повторяющихся чисел для author>>>
+// https://www.youtube.com/watch?v=WUOOMVaZRe4&ab_channel=KodeBase
 
-function swapAndRoundNums(one, two) {
-  one = Math.ceil(one);
-  two = Math.floor(two);
+function getNum (min, max) {
+  const step1 = max - min + 1;
+  const step2 = Math.random() * step1;
 
-  if (one > two) {
-    const swap = one;
-    one = two;
-    two = swap;
+  const result = Math.floor(step2) + min;
+
+  return result;
+}
+
+function createArrayOfNums(start, end) {
+  const myArray = [];
+  // eslint-disable-next-line id-length
+  for(let i = start; i <= end; i++) {
+    myArray.push(i);
   }
 
-  //https://stackoverflow.com/questions/27142657/how-to-write-a-function-that-returns-an-error-in-javascript
-  if (one < 0 || one === two) {
-    throw new Error('Числа не должны быть отрицательными или равными друг-другу.');
+  return myArray;
+}
+
+const numsArray = createArrayOfNums(1, 10);
+
+function getNonRepeatingNums () {
+  const randomIndex = getNum(0, numsArray.length-1);
+  const randomNum = numsArray[randomIndex];
+
+  numsArray.splice(randomIndex, 1);
+  if(randomNum < 10) {
+    return `0${  randomNum}`;
   }
 
-}
-
-function getRandomWholeNum(min, max) {
-
-  swapAndRoundNums(min, max);
-
-  return Math.floor(Math.random() * (max - min)) + min;
+  return randomNum;
 }
 
 
-function getRandomNumWithDecimals(min, max, decimals) {
+// <<<Функции для свойств объекта offer>>>
 
-  swapAndRoundNums(min, max);
+function getPlace () {
+  const places = ['palace', 'house', 'bungalow', 'hotel', 'flat'];
+  const place = places[Math.floor(Math.random()*places.length)];
 
-  return Number((Math.random() * (max - min) + min).toFixed(decimals));
+  return place;
 }
 
-getRandomWholeNum(1, 2);
-getRandomNumWithDecimals(1, 2, 3);
+function getCheckInOut() {
+  const checks = ['12:00', '13:00', '14:00'];
+  const checkInOut = checks[Math.floor(Math.random()*checks.length)];
+
+  return checkInOut;
+}
+
+function getRandomFeats () {
+  const feats = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  const randomFeats = feats.slice(getRandomPositiveInteger(0, feats.length));
+
+  return randomFeats;
+}
+
+function getRandomPhotos () {
+  const photos = [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  ];
+  const randomPhotos = photos.slice(getRandomPositiveInteger(0, photos.length - 1));
+
+  return randomPhotos;
+}
+
+function getRandomTitle () {
+  const titles = ['Прекрасный вид!', 'Удобное расположение!', 'Низкая цена!', 'Лучшее предложение!'];
+  const randomTitle = titles[Math.floor(Math.random()*titles.length)];
+
+  return randomTitle;
+}
+
+
+// <<<Функция для возвращения массива из 10 предложений>>>
+
+function getOffers () {
+
+  const tenOffers = [];
+  // eslint-disable-next-line id-length
+  for (let i = 0; i < 10; i++) {
+    const author = {
+      avatar: `img/avatars/user${  getNonRepeatingNums()  }.png`,
+    };
+
+    const location = {
+      lat: getRandomPositiveFloat(35.65, 35.7, 5),
+      lng: getRandomPositiveFloat(139.7, 139.8, 5),
+    };
+
+    const offer = {
+      title: getRandomTitle(),
+      address: (`${location.lat  }, ${  location.lng}`),
+      price: getRandomPositiveInteger(1000, 10000),
+      type: getPlace(),
+      rooms: getRandomPositiveInteger(1, 5),
+      guests: getRandomPositiveInteger(1, 5),
+      checkin: getCheckInOut(),
+      checkout: getCheckInOut(),
+      features: getRandomFeats(),
+      description: '',
+      photos: getRandomPhotos(),
+    };
+
+    const description = function getDescription() {
+      let descriptionText = '';
+
+      if (offer.type === 'palace') {
+        descriptionText = 'Шикарный дворец.';
+      } else if (offer.type === 'flat') {
+        descriptionText = 'Хорошая квартира.';
+      } else if (offer.type === 'house') {
+        descriptionText = 'Теплый дом.';
+      } else if (offer.type === 'bungalow') {
+        descriptionText = 'Уютное бунгало.';
+      } else if (offer.type === 'hotel') {
+        descriptionText = 'Популярный отель.';
+      }
+
+      offer.description = descriptionText;
+    };
+
+    description();
+
+    const authorPlusOffer = Object.assign({}, author, offer);
+    tenOffers.push(authorPlusOffer);
+  }
+
+  return tenOffers;
+}
+
+getOffers();
