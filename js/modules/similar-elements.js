22079398c0f8e692703ca1968379d22552c48ba8
Al Much <almuchwork@gmail.com>
2021-10-31 17:05:52 +0300

    -

--- js/modules/similar-elements.js
+++ js/modules/similar-elements.js
@@ -2,36 +2,89 @@
 
 import * as data from './data.js';
 
+let offer = data.getOffers()[0];
+
+let title       = offer.offer.title;
+let address     = offer.offer.address;
+let price       = offer.offer.price;
+let type        = offer.offer.type;
+let rooms       = offer.offer.rooms;
+let guests      = offer.offer.guests;
+let checkin     = offer.offer.checkin;
+let checkout    = offer.offer.checkout;
+let features    = offer.offer.features;
+let description = offer.offer.description;
+let photos      = offer.offer.photos;
+let avatar      = offer.author.avatar;
+
 const cardTemplate = document.querySelector('#card').content;
-const popupTemplate = cardTemplate.querySelector('.popup');
+let popupClone = cardTemplate.querySelector('.popup').cloneNode(true);
 
-let offer = data.getOffers();
-
-let title = offer[0].offer.title;
-let address = offer[0].offer.address;
-let price = offer[0].offer.price;
-let type = offer[0].offer.type;
-let rooms = offer[0].offer.rooms;
-let guests = offer[0].offer.guests;
-let features = offer[0].offer.features;
-let description = offer[0].offer.description;
-let photos = offer[0].offer.photos;
-let avatar = offer[0].author.avatar;
+popupClone.querySelector('.popup__title').innerHTML = title;
+popupClone.querySelector('.popup__text--address').innerHTML = address;
+popupClone.querySelector('.popup__text--price').innerHTML = price + ' ₽/ночь';
+popupClone.querySelector('.popup__text--capacity').innerHTML = rooms + ' комнаты для ' + guests + ' гостей';
+popupClone.querySelector('.popup__text--time').innerHTML = 'Заезд после ' + checkin + ', выезд до ' + checkout;
+popupClone.querySelector('.popup__description').innerHTML = description;
 
 
-let popupClone = popupTemplate.cloneNode(true);
+// <<< Сопоставление типов жилья с подписями >>>
 
-popupClone.children[1].textContent = title;
-popupClone.children[2].textContent = address;
+let typeTitle;
+let getTypeTitle = function (type) {
+
+  if (type === 'flat') {
+    typeTitle = 'Квартира';
+
+  } else if (type === 'bungalow') {
+    typeTitle = 'Бунгало';
+
+  } else if (type === 'house') {
+    typeTitle = 'Дом';
+
+  } else if (type === 'palace') {
+    typeTitle = 'Дворец';
+
+  } else if (type === 'hotel') {
+    typeTitle = 'Отель';
+
+  } else { typeTitle = ''}
+
+return typeTitle;
+};
+
+getTypeTitle(type);
+
+popupClone.querySelector('.popup__type').innerHTML = typeTitle;
 
 
+// <<< Выведение удобств в объявление >>>
+// Применил решение из демонстрации 7.8
+
+const featureContainer = popupClone.querySelector('.popup__features');
+const featureListFragment = document.createDocumentFragment();
+
+features.forEach ((feature) => {
+  const featureListItem = featureContainer.querySelector('.popup__feature--' + feature);
+  featureListFragment.append(featureListItem);
+})
 
 
+// <<< Выведение фотографий >>>
 
-/*
-console.log(offer);
-console.log(popupTemplate);
-console.log(title);
-console.log(popupTemplate.children)
-console.log(popupClone);
-*/
+const photoContainer = popupClone.querySelector('.popup__photos');
+const photoListFragment = document.createDocumentFragment();
+
+  for (let i = 0; i < photos.length; i++ ) {
+  let photoListItem = photoContainer.querySelector('.popup__photo').cloneNode(true);
+  photoListItem.src = photos[i];
+  photoListFragment.append(photoListItem);
+  photoContainer.append(photoListFragment);
+}
+
+let notice = document.querySelector('.notice');
+notice.appendChild(popupClone);
+
+// Почему avatar = "img/avatars/userundefined.png"? Точнее, почему GetNonRepetingNums выдает undefined?
+popupClone.querySelector('.popup__avatar').src = avatar;
+console.log(data.getOffers()[0]);
